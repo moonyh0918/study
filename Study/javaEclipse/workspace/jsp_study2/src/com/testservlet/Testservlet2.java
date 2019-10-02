07d6ce62e077b87f8d575f2a1e05070a69ad0fb3
moonyh <jaesmooon@naver.com>
2019-10-02 14:49:03 +0900

    jsp_study(jspMapping)
    jsp맵핑 방법 및 실습
    1.xml파일을 이용한 방식
    2.annotation을 이용한 방식

---
+++ Study/javaEclipse/workspace/jsp_study2/src/com/testservlet/Testservlet2.java
@@ -0,0 +1,53 @@
+package com.testservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Testservlet2
+ */
+@WebServlet("/Testservlet2")
+public class Testservlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Testservlet2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		PrintWriter out = response.getWriter();
+		out.print("<!DOCTYPE html>");
+		out.print("<html>");
+		out.print("<head>");
+		out.print("</head>");
+		out.print("<body>");
+		out.print("<p>Hello world!</p>");
+		out.print("</body>");
+		out.print("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
